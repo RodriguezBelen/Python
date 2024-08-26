dc787364c665a84ef28ac683a39fddfd212dc4e8
Rodriguezbelen <beelu2.1997@gmail.com>
2024-08-25 22:11:17 -0300

    Inicialización de Python

---
+++ hello.py
@@ -0,0 +1,13 @@
+print ("Hola mundo"),
+#probamos colocar mis datos a ver que onda
+
+print ("Soy Belén Rodriguez")
+
+print ("Mi edad es la resta del año en el que estamos, y 1997")
+
+#calculamos la edad
+
+edad = 2024 - 1997
+
+print ("Tengo", edad, "años")
+
---
+++ script_inicial.py
@@ -0,0 +1,19 @@
+print ("Mi primer codigo de python"),
+# Bloque de código si condición es verdadera es "if condition"
+
+#Bloque de código si la condición es falsa es "else"
+
+""" 
+es un comentario
+de varias líneas 
+"""
+#asginamso variables:
+nombre = 'Belen'
+apellido = 'Rodriguez'
+edad = 27
+
+#imprimimos variables: lo que se quiere imprimir va entre parentesis.
+print (nombre, apellido, edad)
+nombre_segundo = "Andrea"
+
+print ("Nombre completo:" + nombre_segundo +' '+ nombre)
